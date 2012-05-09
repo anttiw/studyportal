df97ef0c5b47e3d37f9c3fce78f6353f3f273824
Antti Wilenius <antti.wilenius@gmail.com>
2012-05-09 08:44:10 +0300

    Creating a new group-function started

--- courses.php
+++ courses.php
@@ -27,11 +27,34 @@
 				echo '<form name="createGroup" method="POST" action="courses.php">
 						<p>
 						Group name:</br>
-						<input type="text" name="groupname" /></br>
+						<input type="text" name="groupname" size="27" /></br>
 						Number of members:</br>
-						<input type="text" name="num_members" /></br>
+						<input type="text" name="num_members" size="2" />
+						<input type="submit" name="genGroupList" value="Generate Member List" />
 						</p>
-						</form>';
+						</form>
+						<hr>';
+				
+				if(isset($_POST['genGroupList']))
+				{
+					echo '<p>Testing! Number of members was: ' .$_POST['num_members'].'</p>';
+					
+					$numMembers = $_POST['num_members'];
+					$groupName = $_POST['groupname'];
+					
+					echo '<form name="groupMembers" method="POST" action="courses.php">
+							<p>';
+					
+						// Loop through all of the inserted "members"
+						for($i = 1 ; $i <= $numMembers ; $i++)
+						{
+							echo 'Member '.$i.':</br>
+							<input type="text" name="member'.$i.'" size="27"></br>';
+						}
+					
+					echo '</p>
+							</form>';
+				}
 			
 			}
 		
